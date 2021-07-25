fabae8ea5c661274d081c0af42dd08fd87485fdc
idembele70 <idembele70@gmail.com>
2021-07-25 17:53:21 +0200

    gestion des erreurs

--- src/components/AddTodo.jsx
+++ src/components/AddTodo.jsx
@@ -1,10 +1,14 @@
-import { Box, Button, Grid, makeStyles, TextField, ThemeProvider } from "@material-ui/core"
-import React, { useRef } from "react"
+import { Box, Button, Grid, TextField, ThemeProvider } from "@material-ui/core"
+import React, { useRef, useState } from "react"
 import theme from "../values/theme.js"
 export default function AddTodo({ onAddTodo }) {
   const msgRef = useRef(null)
+  const [error, setError] = useState(false)
   const handleAddTodo = (e) => {
     e.preventDefault()
+    if(!msgRef.current.value) 
+    return setError(true)
+    if(error) setError(false)
     onAddTodo(msgRef.current.value)
     msgRef.current.value = null
   }
@@ -12,7 +16,7 @@ export default function AddTodo({ onAddTodo }) {
   return <Grid item xs={6}>
     <Box boxShadow={2} p={4}>
       <form autoComplete="off">
-        <TextField id="standard-basic" label="Add new todo" fullWidth variant="outlined" inputRef={msgRef} />
+        <TextField id={"standard-"+(error ?"error" : "basic")} error={error} label="Add new todo" fullWidth variant="outlined" inputRef={msgRef} helperText={error ? "this input value cannot be null" : ""} />
         <Box m="8px 0" >
           <ThemeProvider theme={theme}>
             <Grid container justifyContent="center">
